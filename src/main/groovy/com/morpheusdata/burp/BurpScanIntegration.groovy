ff6afc10487c0b0ddfc7c4791c69aefadb014f22
Uthman Al-Ayek Eqbal <135247149+uthm4n@users.noreply.github.com>
2023-07-18 21:56:35 +0100

    Add files via upload

---
+++ src/main/groovy/com/morpheusdata/burp/BurpScanIntegration.groovy
@@ -0,0 +1,58 @@
+package com.morpheusdata.BurpScanIntegration;
+
+import com.morpheusdata.core.Plugin;
+import com.morpheusdata.model.OptionType;
+import com.morpheusdata.model.OptionType.InputType;
+import java.util.Arrays;
+
+class BurpScanIntegration extends Plugin {
+
+    public static final OptionType burpApiUrl = getBurpApiURL();
+    public static final OptionType burpApiKey = getBurpApiKey();
+
+
+    @Override
+    String getCode() {
+        return "burp-scan-integration";
+    }
+
+    @Override
+    void initialize() {
+        this.setName("BurpScan");
+        this.settings.add(burpApiUrl);
+        this.settings.add(burpApiKey);
+
+    }
+
+    private static OptionType getBurpApiURL() {
+        OptionType burpApiUrl = new OptionType();
+        burpApiUrl.setName("REST API URL");
+        burpApiUrl.setCode("burp-api-url");
+        burpApiUrl.setFieldName("burpApiURL");
+        burpApiUrl.setFieldLabel(burpApiUrl.getName());
+        burpApiUrl.setDisplayOrder(0);
+        burpApiUrl.setHelpText("Burp Suite REST API URL");
+        burpApiUrl.setRequired(true);
+        burpApiUrl.setInputType(InputType.TEXT);
+        return burpApiUrl;
+
+    }
+    private static OptionType getBurpApiKey() {
+        OptionType burpApiKey = new OptionType();
+        burpApiKey.setName("API Key");
+        burpApiKey.setCode("burp-api-key");
+        burpApiKey.setFieldName("burpApiKey");
+        burpApiKey.setFieldLabel(burpApiKey.getName());
+        burpApiKey.setDisplayOrder(1);
+        burpApiKey.setHelpText("Burp Suite REST API Key");
+        burpApiKey.setRequired(true);
+        burpApiKey.setInputType(InputType.TEXT);
+        return burpApiKey;
+
+    }
+
+    @Override
+    void onDestroy() {
+
+    }
+}
