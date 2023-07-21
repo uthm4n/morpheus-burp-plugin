02c076e7498ab6f155129e9601c08dfd6711cab8
Uthman Al-Ayek Eqbal <uthman@Uthmans-MacBook-Pro.local>
2023-07-21 14:25:38 +0100

    Add logos and BurpScanTaskProvider.groovy

--- src/main/groovy/com/morpheusdata/burp/BurpScanTaskPlugin.groovy
+++ src/main/groovy/com/morpheusdata/burp/BurpScanTaskPlugin.groovy
@@ -7,10 +7,6 @@ import java.util.Arrays;
 
 class BurpScanTaskPlugin extends Plugin {
 
-    public static final OptionType burpApiUrl = getBurpApiURL();
-    public static final OptionType burpApiKey = getBurpApiKey();
-
-
     @Override
     String getCode() {
         return "burp-scan-task";
@@ -18,6 +14,8 @@ class BurpScanTaskPlugin extends Plugin {
 
     @Override
     void initialize() {
+        BurpScanTaskProvider BurpScanTaskProvider = new BurpScanTaskProvider(this, morpheus)
+		this.pluginProviders.put("burp", BurpScanTaskProvider)
         this.setName("Burp Scan");
         this.settings.add(burpApiUrl);
         this.settings.add(burpApiKey);
@@ -26,35 +24,8 @@ class BurpScanTaskPlugin extends Plugin {
 
     }
 
-    private static OptionType getBurpApiURL() {
-        OptionType burpApiUrl = new OptionType();
-        burpApiUrl.setName("REST API URL");
-        burpApiUrl.setCode("burp-api-url");
-        burpApiUrl.setFieldName("burpApiURL");
-        burpApiUrl.setFieldLabel(burpApiUrl.getName());
-        burpApiUrl.setDisplayOrder(0);
-        burpApiUrl.setHelpText("Burp Suite REST API URL");
-        burpApiUrl.setRequired(true);
-        burpApiUrl.setInputType(InputType.TEXT);
-        return burpApiUrl;
-
-    }
-    private static OptionType getBurpApiKey() {
-        OptionType burpApiKey = new OptionType();
-        burpApiKey.setName("API Key");
-        burpApiKey.setCode("burp-api-key");
-        burpApiKey.setFieldName("burpApiKey");
-        burpApiKey.setFieldLabel(burpApiKey.getName());
-        burpApiKey.setDisplayOrder(1);
-        burpApiKey.setHelpText("Burp Suite REST API Key");
-        burpApiKey.setRequired(true);
-        burpApiKey.setInputType(InputType.TEXT);
-        return burpApiKey;
-
-    }
-
     @Override
     void onDestroy() {
-
+        morpheus.task.disableTask('burp').blockingGet()
     }
 }
--- src/main/groovy/com/morpheusdata/burp/BurpScanTaskProvider.groovy
+++ src/main/groovy/com/morpheusdata/burp/BurpScanTaskProvider.groovy
@@ -1,1 +1,97 @@
+package com.morpheusdata.burp
 
+import com.morpheusdata.core.AbstractTaskService
+import com.morpheusdata.core.ExecutableTaskInterface
+import com.morpheusdata.core.MorpheusContext
+import com.morpheusdata.core.Plugin
+import com.morpheusdata.core.TaskProvider
+import com.morpheusdata.model.Icon
+import com.morpheusdata.model.OptionType
+import com.morpheusdata.model.TaskType
+
+class BurpScanTaskProvider implements TaskProvider {
+    MorpheusContext morpheusContext
+    Plugin plugin
+    AbstractTaskService service
+
+    BurpScanTaskProvider(Plugin plugin, MorpheusContext morpheusContext) {
+        this.plugin = plugin
+        this.morpheusContext = morpheusContext
+        this.service = new BurpScanTaskService(morpheus)
+    }
+
+    @Override
+    TaskType.TaskScope getScope() {
+        return TaskType.TaskScope.all
+    }
+
+    @Override
+    String getDescription() {
+        return "Trigger a Burp Scan from within Morpheus and see the results in the UI"
+    }
+
+    @Override
+    Boolean isAllowExecuteLocal() {
+        return true
+    }
+
+    @Override
+    Boolean isAllowExecuteRemote() {
+        return false
+    }
+
+    @Override
+    Boolean isAllowExecuteResource() {
+        return false
+    }
+
+    @Override
+    Boolean isAllowLocalRepo() {
+        return false
+    }
+
+    @Override
+    Boolean isAllowRemoteKeyAuth() {
+        return false
+    }
+
+    @Override
+    Boolean hasResults() {
+        return true
+    }
+
+    @Override
+    List<OptionType> getOptionTypes() {
+        return [
+                new OptionType(code: 'burp.apiUrl', name: 'Burp API URL', inputType: OptionType.InputType.TEXT, fieldName: 'apiUrl', fieldLabel: 'REST API Url', displayOrder: 0),
+                new OptionType(code: 'burp.apiKey', name: 'Burp API Key', inputType: OptionType.InputType.TEXT, fieldName: 'apiKey', fieldLabel: 'API Key', displayOrder: 1),
+                new OptionType(code: 'burp.scanConfiguration', name: 'Scan Configuration (JSON)', inputType: OptionType.InputType.CODE_EDITOR, fieldName: 'scanConfiguration', fieldLabel: 'Scan Configuration', displayOrder: 2),
+
+        ]
+    }
+
+    @Override
+    MorpheusContext getMorpheus() {
+        return morpheusContext
+    }
+
+    @Override
+    Plugin getPlugin() {
+        return plugin
+    }
+
+    @Override
+    String getCode() {
+        return 'burpScan'
+    }
+
+    @Override
+    String getName() {
+        return 'Burp Scan Task'
+    }
+
+    @Override
+    Icon getIcon() {
+        return new Icon(path:"burp-white.svg", darkPath: "burp-black.svg") // edit logo 
+    }
+}
