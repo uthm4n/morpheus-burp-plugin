231f8a459a1812c96a251375542f262117d85e62
Uthman Al-Ayek Eqbal <135247149+uthm4n@users.noreply.github.com>
2023-07-21 17:48:04 +0100

    Remove this.settings for burpApiUrl and burpApiKey - these are now embedded in the task configuration options

--- src/main/groovy/com/morpheusdata/burp/BurpScanTaskPlugin.groovy
+++ src/main/groovy/com/morpheusdata/burp/BurpScanTaskPlugin.groovy
@@ -17,8 +17,6 @@ class BurpScanTaskPlugin extends Plugin {
         BurpScanTaskProvider BurpScanTaskProvider = new BurpScanTaskProvider(this, morpheus)
 	this.pluginProviders.put("burp", BurpScanTaskProvider)
         this.setName("Burp Scan");
-        this.settings.add(burpApiUrl);
-        this.settings.add(burpApiKey);
         this.setAuthor("Uthman Al-Ayek Eqbal");
         this.setDescription("Trigger a Burp Scan from the Morpheus UI");
 
