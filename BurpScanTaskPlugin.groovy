7f9f10e7e780db30218b8f4aee25a2da1dd7e059
Uthman Al-Ayek Eqbal <135247149+uthm4n@users.noreply.github.com>
2023-09-04 17:39:05 +0100

    Latest

---
+++ BurpScanTaskPlugin.groovy
@@ -0,0 +1,33 @@
+package com.morpheusdata.burp;
+
+import com.morpheusdata.core.Plugin;
+import com.morpheusdata.model.Permission;
+import com.morpheusdata.model.OptionType;
+import com.morpheusdata.model.OptionType.InputType;
+import java.util.Arrays;
+
+class BurpScanTaskPlugin extends Plugin {
+
+    @Override
+    String getCode() {
+        return "burp-scan-task";
+    }
+
+    @Override
+    void initialize() {
+        BurpScanTaskProvider BurpScanTaskProvider = new BurpScanTaskProvider(this, morpheus)
+	    this.pluginProviders.put(BurpScanTaskProvider.getCode(), BurpScanTaskProvider)
+        BurpOptionSourceProvider optionSourceProvider = new BurpOptionSourceProvider(this,morpheus)
+		this.pluginProviders.put(optionSourceProvider.getCode(),optionSourceProvider)
+        this.setName("Burp Scan");
+        this.setAuthor("Uthman Al-Ayek Eqbal");
+        this.setDescription("Trigger a Burp Scan from the Morpheus UI")
+        this.setPermissions([Permission.build('Burp Scan Task','burp-scan-task-permission', 'security', [Permission.AccessType.none, Permission.AccessType.full])])
+
+    }
+
+    @Override
+    void onDestroy() {
+        morpheus.task.disableTask('burp').blockingGet()
+    }
+}
