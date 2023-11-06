ebfde3b5dea933f27e2c30bdea7f622db5be831f
Uthman Al-Ayek Eqbal <135247149+uthm4n@users.noreply.github.com>
2023-11-06 17:46:41 +0000

    fix comment
    this isn't python.  # replaced with //

--- src/main/groovy/com/morpheusdata/burp/BurpScanTaskService.groovy
+++ src/main/groovy/com/morpheusdata/burp/BurpScanTaskService.groovy
@@ -82,7 +82,7 @@ class BurpScanTaskService extends AbstractTaskService {
             } else if (scanConfigType == 'Custom') {
                 StringEscapeUtils stringEscape = new StringEscapeUtils()
                 String escapedJSONConfig = stringEscape.escapeJava(customScanConfig)
-                String finalCustomConfig = escapedJSONConfig.replaceAll("\\n", "")  # remove the newline characters in preparation for injection in the http body - fussy burp stuff 
+                String finalCustomConfig = escapedJSONConfig.replaceAll("\\n", "")  // remove the newline characters in preparation for injection in the http body - fussy burp stuff 
                 def body = [
                         'scan_configurations' : [
                             ['config': finalCustomConfig, 'type': 'CustomConfiguration']
